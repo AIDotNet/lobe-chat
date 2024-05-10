f5b73a443327aeb1b6c5c6b89d76a86d7c984887
token <239573049@qq.com>
2024-05-10 17:20:25 +0800

    修改

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -6,7 +6,7 @@ import { OFFICIAL_URL } from '@/const/url';
 
 import pkg from '../../package.json';
 
-const title = 'LobeChat';
+const title = 'FastWki-Chat';
 const { description, homepage } = pkg;
 
 const { SITE_URL = OFFICIAL_URL } = getServerConfig();
@@ -53,7 +53,7 @@ const metadata: Metadata = {
 
   title: {
     default: title,
-    template: '%s · LobeChat',
+    template: '%s · FastWki-Chat',
   },
   twitter: {
     card: 'summary_large_image',
--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -2,7 +2,7 @@ import { memo, useEffect } from 'react';
 
 const PageTitle = memo<{ title: string }>(({ title }) => {
   useEffect(() => {
-    document.title = title ? `${title} · LobeChat` : 'LobeChat';
+    document.title = title ? `${title} · FastWki-Chat` : 'FastWki-Chat';
   }, [title]);
 
   return null;
